09ed8c750039890a4cd717239d33d19e8dd0da10
Waleed Elginady <waleedelginady@gmail.com>
2023-03-13 18:47:26 +0000

    sub

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -2,10 +2,9 @@ from datetime import datetime, timedelta, timezone
 from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
+    #with xray_recorder.in_segment('segment_name') as segment:
     # x ray -----
-    #segment = xray_recorder.begin_segment('user_activities')
-    
-    
+    #segment = xray_recorder.begin_segment('user_activities') 
     model = {
       'errors': None,
       'data': None
@@ -25,4 +24,12 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
+
+    subsegment = xray_recorder.begin_subsegment('mock-data')
+    # xray ----------
+    dict = {
+      "now": now.isoformat()
+      "results-size": len(model['data'])
+    }
+    subsegment.put_metadata('key', dict, 'namespace')
     return model
