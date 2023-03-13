ce92e6e028cde8e04de243806f5a829cb33fb15c
Waleed Elginady <waleedelginady@gmail.com>
2023-03-13 18:46:59 +0000

    subsegments

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
-#
+
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
