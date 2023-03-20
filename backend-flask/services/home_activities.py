b3483619017311603d8bf5713f48069da7cc0dac
Waleed Elginady <waleedelginady@gmail.com>
2023-03-20 22:24:57 +0000

    pug

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,26 +1,26 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-import logging
 
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run():
+  def run(cognito_user_id=None):
     #logger.info("HomeActivities")
-    with tracer.start_as_current_span("home-activities-mock-data"):
+    with tracer.start_as_current_span("home-activites-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
+
       results = [{
-          'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-          'handle':  'Andrew Brown',
-          'message': 'Cloud is fun!',
-          'created_at': (now - timedelta(days=2)).isoformat(),
-          'expires_at': (now + timedelta(days=5)).isoformat(),
-          'likes_count': 5,
-          'replies_count': 1,
-          'reposts_count': 0,
-          'replies': [{
+        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'Andrew Brown',
+        'message': 'Cloud is very fun!',
+        'created_at': (now - timedelta(days=2)).isoformat(),
+        'expires_at': (now + timedelta(days=5)).isoformat(),
+        'likes_count': 5,
+        'replies_count': 1,
+        'reposts_count': 0,
+        'replies': [{
           'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
           'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
           'handle':  'Worf',
@@ -61,7 +61,7 @@ class HomeActivities:
           'likes': 1042,
           'replies': []
         }
+        results.insert(0,extra_crud)
 
-      results.insert(0,extra_crud)
       span.set_attribute("app.result_length", len(results))
       return results
