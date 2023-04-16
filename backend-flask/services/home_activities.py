b13aed9ac9e6f6a83f56bb6d5fa241df5897e219
Waleed Elginady <waleedelginady@gmail.com>
2023-04-16 10:45:13 +0000

    home act

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,16 +4,15 @@ from lib.db import pool, query_wrap_object, query_wrap_array
 
 # tracer = trace.get_tracer("home.activities")
 
+
 class HomeActivities:
   def run(cognito_user_id=None):
-    print("HOME ACTIVITY")
     #logger.info("HomeActivities")
     # with tracer.start_as_current_span("home-activites-mock-data"):
     #   span = trace.get_current_span()
     #   now = datetime.now(timezone.utc).astimezone()
     #   span.set_attribute("app.now", now.isoformat())
-
-    sql = query_wrap_array("""
+    results = db.query_array_json("""
       SELECT
         activities.uuid,
         users.display_name,
