3d1371992c488fbfd521286e5c141e1ec43a6194
Waleed Elginady <waleedelginady@gmail.com>
2023-05-05 13:53:19 +0000

    messageform

--- frontend-react-js/src/components/MessageForm.js
+++ frontend-react-js/src/components/MessageForm.js
@@ -1,49 +1,44 @@
-import './MessageForm.css';
+import './ActivityForm.css';
 import React from "react";
 import process from 'process';
-import { json, useParams } from 'react-router-dom';
+import {ReactComponent as BombIcon} from './svg/bomb.svg';
 
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
   const [message, setMessage] = React.useState('');
-  const params = useParams();
+  const [ttl, setTtl] = React.useState('7-days');
 
   const classes = []
   classes.push('count')
-  if (1024-count < 0){
+  if (240-count < 0){
     classes.push('err')
   }
 
   const onsubmit = async (event) => {
     event.preventDefault();
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities`
       console.log('onsubmit payload', message)
-      let json = { 'message': message }
-      if (params.handle) {
-        json.handle = params.handle
-      } else {
-        json.message_group_uuid = params.message_group_uuid
-      }
-
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
-          'Authorization': `Bearer ${localStorage.getItem("access_token")}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(json)
+        body: JSON.stringify({
+          message: message,
+          ttl: ttl
+        }),
       });
       let data = await res.json();
       if (res.status === 200) {
-        console.log('data:',data)
-        if (data.message_group_uuid) {
-          console.log('redirect to message group')
-          window.location.href = `/messages/${data.message_group_uuid}`
-        } else {
-          props.setMessages(current => [...current,data]);
-        }
+        // add activity to the feed
+        props.setActivities(current => [data,...current]);
+        // reset and close the form
+        setCount(0)
+        setMessage('')
+        setTtl('7-days')
+        props.setPopped(false)
       } else {
         console.log(res)
       }
@@ -57,21 +52,42 @@ export default function ActivityForm(props) {
     setMessage(event.target.value);
   }
 
-  return (
-    <form 
-      className='message_form'
-      onSubmit={onsubmit}
-    >
-      <textarea
-        type="text"
-        placeholder="send a direct message..."
-        value={message}
-        onChange={textarea_onchange} 
-      />
-      <div className='submit'>
-        <div className={classes.join(' ')}>{1024-count}</div>
-        <button type='submit'>Message</button>
-      </div>
-    </form>
-  );
+  const ttl_onchange = (event) => {
+    setTtl(event.target.value);
+  }
+
+  if (props.popped === true) {
+    return (
+      <form 
+        className='activity_form'
+        onSubmit={onsubmit}
+      >
+        <textarea
+          type="text"
+          placeholder="what would you like to say?"
+          value={message}
+          onChange={textarea_onchange} 
+        />
+        <div className='submit'>
+          <div className={classes.join(' ')}>{240-count}</div>
+          <button type='submit'>Crud</button>
+          <div className='expires_at_field'>
+            <BombIcon className='icon' />
+            <select
+              value={ttl}
+              onChange={ttl_onchange} 
+            >
+              <option value='30-days'>30 days</option>
+              <option value='7-days'>7 days</option>
+              <option value='3-days'>3 days</option>
+              <option value='1-day'>1 day</option>
+              <option value='12-hours'>12 hours</option>
+              <option value='3-hours'>3 hours</option>
+              <option value='1-hour'>1 hour </option>
+            </select>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
